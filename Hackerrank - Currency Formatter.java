ef3da97b486da84e661bf214227707c0d759fb41
Akhil Nair <86709119+drachenherz19@users.noreply.github.com>
2023-10-05 21:07:51 -0400

    Add files via upload

---
+++ Hackerrank - Currency Formatter.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Locale;
+import java.text.NumberFormat;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        Locale indialocale = new Locale ("en", "IN");
+        
+        NumberFormat us = NumberFormat.getCurrencyInstance (Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance (indialocale);
+        NumberFormat china = NumberFormat.getCurrencyInstance (Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance (Locale.FRANCE);
+        // Write your code here.
+        
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
