c5436b5421df50431f8e6cef9692ca9b122e84a2
Akhil Nair <86709119+drachenherz19@users.noreply.github.com>
2023-10-10 22:16:23 -0400

    Delete Hackerrank - Quicksort 1.py

--- Hackerrank - Quicksort 1.py
+++
@@ -1,9 +0,0 @@
-def quickSort(arr):
-    left, right = [], []
-    for i in range(1, len(arr)):
-        if arr[0] > arr[i]:
-            left.append(arr[i])
-        else:
-            right.append(arr[i])
-    left.append(arr[0])
-    return left + right
