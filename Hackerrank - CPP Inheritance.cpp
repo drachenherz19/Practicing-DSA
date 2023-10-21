d881e4d93b839e303960a2a9f5a4cdddcb43e6d8
Akhil Nair <86709119+drachenherz19@users.noreply.github.com>
2023-10-21 02:53:28 -0400

    Add files via upload

---
+++ Hackerrank - CPP Inheritance.cpp
@@ -0,0 +1,9 @@
+class Isosceles : public Triangle{
+    public:
+    void description(){
+        cout<<"In an isosceles triangle two sides are equal"<<endl;
+    }
+    	void isosceles(){
+    		cout<<"I am an isosceles triangle\n";
+    	}
+};
