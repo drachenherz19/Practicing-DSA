7f39ea6b63ff0b1a0f3f018e9394c1d45787f841
Akhil Nair <86709119+drachenherz19@users.noreply.github.com>
2023-11-29 23:38:00 -0500

    Add files via upload

---
+++ Hackerrank - Append and Delete.py
@@ -0,0 +1,16 @@
+def appendAndDelete(s, t, k):
+    if len(s) + len(t) <= k:
+        return 'Yes'
+    
+    diff_index = min(len(t), len(s))
+    # find where they differ
+    for i in range(0, min(len(t), len(s))):
+        if s[i] != t[i]:
+            diff_index = i
+            break
+         
+    ops = k
+    ops -= len(s) - diff_index
+    ops -= len(t) - diff_index
+    
+    return 'Yes' if ops >= 0 and ops % 2 == 0 else 'No'
