cfbcdfca61ff9689dc40bf96b3b9f1f4bf0f8ea5
Akhil Nair <86709119+drachenherz19@users.noreply.github.com>
2024-07-05 19:04:24 -0400

    Add files via upload

---
+++ Majority Element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = {}
+        res, maxcount = 0,0
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+            res = n if count[n] > maxcount else res
+            maxcount = max(count[n], maxcount)
+        return res
